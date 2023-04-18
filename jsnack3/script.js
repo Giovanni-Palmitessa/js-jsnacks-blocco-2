a14e9a3b051858bf41c8b13aedb80f6cd8524fa5
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-18 18:16:31 +0200

    add console.log for sum

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -12,7 +12,7 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20, 11, 12, 13, 14, 15]
  
 let sum = 0;
 
-for (let i = 1; i < numbers.length; i = i + 2) {
+for (let i = 0; i < numbers.length; i = i + 2) {
     
     sum+= numbers[i];
 }
