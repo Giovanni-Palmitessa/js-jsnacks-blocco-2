b6e0d98204f776e4e38e6e667a1e4f0dbd5d99d2
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-18 15:32:15 +0200

    add for

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -8,9 +8,5 @@ FINE SE
 
 const randomNumbers = [''];
 
-const number1 = parseInt(prompt('Dammi il primo numero:'));
-const number2 = parseInt(prompt('Dammi il secondo numero:'));
-const number3 = parseInt(prompt('Dammi il terzo numero:'));
-const number4 = parseInt(prompt('Dammi il quarto numero:'));
-const number5 = parseInt(prompt('Dammi il quinto numero:'));
-const number6 = parseInt(prompt('Dammi il sesto numero:'));
+for (let )
+
