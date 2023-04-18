e3a01678ff5d2e9e3f2e2432bdb748f34f04f6ee
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-18 16:20:26 +0200

    add for cycle

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -7,3 +7,10 @@ creare un array vuoto
 
 const randomNames = ['Antonio', 'Giorgio', 'Paolo', 'Francesca', 'Fabio'];
 
+const randomSurnames = ['Spada', 'Rossi', 'Vinci', 'Ferro', 'Campo'];
+
+const randomInvented = [];
+
+for (let i = 0; i < randomNames.length; i++) {
+    
+}
