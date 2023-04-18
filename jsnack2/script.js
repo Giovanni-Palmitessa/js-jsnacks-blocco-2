4e7cd147bb448356cba1d983699bc86bff7c4be6
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-18 16:14:10 +0200

    add array for names

---
+++ jsnack2/script.js
@@ -0,0 +1,9 @@
+/*
+creare un array con nomi inventati
+creare un array con cognomi inventati
+creare un array vuoto
+
+*/
+
+const randomNames = ['Antonio', 'Giorgio', 'Paolo', 'Francesca', 'Fabio'];
+
