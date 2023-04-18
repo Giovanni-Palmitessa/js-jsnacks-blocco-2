4502a879bfe5d68d6aebd05c8f550e479dcfb6f2
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-18 17:02:33 +0200

    add cycle for random numbers

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,6 +2,8 @@
 creare un array con nomi inventati
 creare un array con cognomi inventati
 creare un array vuoto
+creare un generatore random di numeri per i nomi
+creare un generatore random di numeri per i cognomi
 
 */
 
@@ -11,5 +13,13 @@ const randomSurnames = ['Spada', 'Rossi', 'Vinci', 'Ferro', 'Campo'];
 
 const randomInvented = [];
 
-const randomInvited = Math.floor(Math.random() * (4 - 0 + 1) ) + 0;
-console.log(randomInvited);
+const randomNumberNames = Math.floor(Math.random() * (4 - 0 + 1) ) + 0;
+console.log(randomNumberNames);
+
+const randomNumberSurnames = Math.floor(Math.random() * (4 - 0 + 1) ) + 0;
+console.log(randomNumberSurnames);
+
+for (let i = 0; i < randomNames.length; i++) {
+    
+}
+
